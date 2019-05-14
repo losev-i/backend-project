f84f088b0b503f7daf39be0bcea0d3a35814b8b7
ReneSpringer <rene.springer@gmx.de>
2019-05-14 13:34:28 +0200

    fix(1207): fixed name of test

--- src/helloWorld/helloWorld.spec.ts
+++ src/helloWorld/helloWorld.spec.ts
@@ -2,7 +2,7 @@ import test from 'ava';
 import { Context } from 'koa';
 import * as hello from './helloWorld';
 
-test('fn home.get', async t => {
+test('fn helloWorld.get', async t => {
   const ctxMock = { body: '' };
 
   await hello.get(ctxMock as Context, () => Promise.resolve());
