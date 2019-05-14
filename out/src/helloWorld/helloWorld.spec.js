516f05e791975e69638b593d434e559b29f5785a
ReneSpringer <rene.springer@gmx.de>
2019-05-14 13:33:42 +0200

    test(1207): for fn helloWorld.get

---
+++ out/src/helloWorld/helloWorld.spec.js
@@ -0,0 +1,27 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ava_1 = __importDefault(require("ava"));
+const hello = __importStar(require("./helloWorld"));
+ava_1.default('fn home.get', (t) => __awaiter(this, void 0, void 0, function* () {
+    const ctxMock = { body: '' };
+    yield hello.get(ctxMock, () => Promise.resolve());
+    t.is(ctxMock.body, 'Hello World!');
+}));
--- src/helloWorld/helloWorld.spec.ts
+++ src/helloWorld/helloWorld.spec.ts
@@ -1,8 +1,11 @@
 import test from 'ava';
-import Context from 'koa';
-import proxyquire from 'proxyquire';
-import { spy } from 'sinon';
+import { Context } from 'koa';
+import * as hello from './helloWorld';
 
-const pathMock = {
-  join: (...args: any[]) => {}
-};
+test('fn home.get', async t => {
+  const ctxMock = { body: '' };
+
+  await hello.get(ctxMock as Context, () => Promise.resolve());
+
+  t.is(ctxMock.body, 'Hello World!');
+});
