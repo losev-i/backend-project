29e045e8e4994a5d623e589a1b677c2c08934698
Leon Meister <Leonmeister97@gmail.com>
2019-05-14 14:12:53 +0200

    refactor(LM-1202): Move start to www.ts

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,7 @@
-import koa from 'koa';
-import routes from './routes';
-import mount from 'koa-mount';
+import koa from "koa";
+import mount from "koa-mount";
 
-import mongoose, { Schema } from 'mongoose';
+import routes from "./routes";
 
-const app = new koa();
-
-
-mongoose.connect('mongodb://localhost/backend-project', {
-    useNewUrlParser: true
-});
-mongoose.set('useCreateIndex', true);
-
-app.listen(3000);
-console.log('server running on port 3000');
+export const app = new koa();
+export default app;
