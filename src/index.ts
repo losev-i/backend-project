14b1265d228534a86f7f9d03b23c09352f1a14cf
ReneSpringer <rene.springer@gmx.de>
2019-05-14 09:58:14 +0200

    chore(RE-1207): add dependencies, structure

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import koa from 'koa';
+import mount from 'koa-mount';
+
+import routes from './routes';
+
+import mongoose from 'mongoose';
+mongoose.connect('mongodb://localhost/ServerSide', {
+  useNewUrlParser: true
+});
+mongoose.set('useCreateIndex', true);
+
+export const app = new koa();
+
+app.use(mount('/', routes));
+
+export default app;
---
+++ src/routes.ts
@@ -0,0 +1,5 @@
+import koa from 'koa';
+
+const app = new koa();
+
+export default app;
