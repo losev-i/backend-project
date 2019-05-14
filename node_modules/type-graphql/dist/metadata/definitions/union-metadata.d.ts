297189423803e8ae292029a7060435bd8710aca6
jannick1423 <jannick.schwartz@gmail.com>
2019-05-14 17:45:04 +0200

    refactor:JS-5796 add new graphql-packages

---
+++ node_modules/type-graphql/dist/metadata/definitions/union-metadata.d.ts
@@ -0,0 +1,10 @@
+import { ClassType, TypeResolver } from "../../interfaces";
+export interface UnionMetadata {
+    classTypes: ClassType[];
+    name: string;
+    description?: string;
+    resolveType?: TypeResolver<any, any>;
+}
+export interface UnionMetadataWithSymbol extends UnionMetadata {
+    symbol: symbol;
+}
