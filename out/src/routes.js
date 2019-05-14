b06405c0a3f5654900217d9fabc06b688c1359c8
ReneSpringer <rene.springer@gmx.de>
2019-05-14 10:23:15 +0200

    fix(RE-1207): fixed db connection

---
+++ out/src/routes.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const koa_1 = __importDefault(require("koa"));
+const app = new koa_1.default();
+exports.default = app;
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import mount from 'koa-mount';
 import routes from './routes';
 
 import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost/ServerSide', {
+mongoose.connect('mongodb://localhost/backendProject', {
   useNewUrlParser: true
 });
 mongoose.set('useCreateIndex', true);
