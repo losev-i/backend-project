081c113d6f3fce2389c42dceca2bb2568d4bac07
jannick1423 <jannick.schwartz@gmail.com>
2019-05-16 13:42:37 +0200

    feat: JS-5796 add typeorm, User.ts and Register.ts

--- bin/www.ts
+++ bin/www.ts
@@ -1,10 +1,14 @@
-import app from "../src/index";
-import _ from "lodash";
+import app from '../src/index';
+import _ from 'lodash';
+import { createConnection } from 'typeorm';
 
 async function start(options: { port?: number }) {
+  await createConnection();
+
   options = _.defaults(options, { port: process.env.PORT || 3000 });
-  await app.listen(options);
-  console.log(`Server listening on port ${options.port}`);
+  await app.listen(options, () => {
+    console.log(`Server listening on http://localhost:${options.port}`);
+  });
 }
 
 start({});
