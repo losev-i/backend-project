3cc23c3cc3a4ff485763f76e6a8befae4402547f
jannick1423 <jannick.schwartz@gmail.com>
2019-05-20 15:33:06 +0200

    refactor:JS-5796 Add Doc, slight refactor

--- src/graphql/modules/user/find/Find.ts
+++ src/graphql/modules/user/find/Find.ts
@@ -1,33 +1,49 @@
 import 'reflect-metadata';
 import { Resolver, Query, Arg } from 'type-graphql';
-import { User } from '../../../entity/User';
-import { getRepository, Repository } from 'typeorm';
+import { User } from '../User';
+import { getRepository } from 'typeorm';
 
+/**
+ * Resolver class
+ */
 @Resolver()
 export class FindResolver {
-  userRepository = new Repository();
-
-  constructor() {
-    this.userRepository = getRepository(User);
-  }
-
+  /**
+   * Query which finds User via given email
+   * @param email Search criterion
+   * @returns User object that matches criterion
+   */
   @Query(returns => User, { nullable: true })
   async findByEmail(@Arg('email', type => String) email: string) {
-    return await this.userRepository.findOne({ email: email });
+    return await getRepository(User).findOne({ email: email });
   }
 
+  /**
+   * Query which finds all Users
+   * @returns Array filled with all users
+   */
   @Query(returns => [User], { nullable: true })
   async findUsers() {
-    return await this.userRepository.find({});
+    return await getRepository(User).find({});
   }
 
+  /**
+   * Query which finds Users via given name (Consisting of firstName + lastName)
+   * @param name Search criterion
+   * @returns User objects that matches criterion
+   */
   @Query(returns => [User], { nullable: true })
   async findByName(@Arg('name', type => String) name: string) {
-    return await this.userRepository.find({ name: name });
+    return await getRepository(User).find({ name: name });
   }
 
+  /**
+   * Query which finds Users via given lastName
+   * @param lastName Search criterion
+   * @returns User objects that matches criterion
+   */
   @Query(returns => [User], { nullable: true })
   async findByLastName(@Arg('lastName', type => String) lastName: string) {
-    return await this.userRepository.find({ lastName: lastName });
+    return await getRepository(User).find({ lastName: lastName });
   }
 }
