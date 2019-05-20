0a3c74e0356c325d68cdd78ecd994a99a308f53f
jannick1423 <jannick.schwartz@gmail.com>
2019-05-20 15:46:28 +0200

    refactor:JS-5796 Add doc, slight refactor

---
+++ src/graphql/modules/user/register/Register.ts
@@ -0,0 +1,45 @@
+import 'reflect-metadata';
+import { Resolver, Mutation, Arg, Query } from 'type-graphql';
+import * as bcrypt from 'bcryptjs';
+
+import { User } from '../User';
+import { RegisterInput } from './RegisterInput';
+
+/**
+ * Resolver class
+ */
+@Resolver()
+export class RegisterResolver {
+  /**
+   * Resolver class needs query, otherwise an error is thrown
+   * Will be replaced
+   */
+  @Query(() => User, { nullable: true })
+  hello(): string {
+    return 'Hello World!';
+  }
+
+  /**
+   * Registration mutation
+   * Has to be extended to include permissions/roles etc. (?)
+   * Hashes the password using bcrypt
+   * @returns registered User
+   */
+  @Mutation(() => User)
+  async register(@Arg('data')
+  {
+    firstName,
+    lastName,
+    email,
+    password
+  }: RegisterInput): Promise<User> {
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: await bcrypt.hash(password, 12)
+    }).save();
+
+    return user;
+  }
+}
