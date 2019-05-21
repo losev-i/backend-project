2f70cf74b13d654a7486413701eafa965e6d7a26
Leon Meister <Leonmeister97@gmail.com>
2019-05-21 11:02:37 +0200

    refactor(LS-1202): Login.ts -> LoginResolver.ts

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { RegisterResolver } from "./modules/user/register/Register";
 import { FindResolver } from "./modules/user/find/FindResolver";
-import { LoginResolver } from "./modules/user/login/Login";
+import { LoginResolver } from "./modules/user/login/LoginResolver";
 
 export async function RootSchema() {
   return await buildSchema({
