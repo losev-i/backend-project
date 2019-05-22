e48f1d5bd329c238b673f7d28d273ec948a75030
LenaMund <lmund@gastro-instruments.de>
2019-05-22 13:20:14 +0200

    refactor(LE-1108): update structure

--- src/helloWorld/routes.ts
+++
@@ -1,9 +0,0 @@
-// import Router from "koa-router";
-
-// import * as helloWorld from "./helloWorld";
-
-// export const router = new Router();
-
-// router.get("/", helloWorld.get);
-
-// export default router;
--- src/routes.ts
+++ src/routes.ts
@@ -1,20 +1,19 @@
-import koa from "koa";
-import bodyparser from "koa-bodyparser";
-import mount from "koa-mount";
+import koa from 'koa';
+import bodyparser from 'koa-bodyparser';
+import mount from 'koa-mount';
 
-import auth from "./auth";
-import home from "./home";
-import login from "./users/login";
-import user from "./users/user";
+import auth from './auth';
+import home from './home';
+import login from './users/login';
+import user from './users/user';
 
-// import helloWorld from "./helloWorld";
 const app = new koa();
-// app.use(bodyparser()).use(mount("/", helloWorld));
+
 app
   .use(bodyparser())
-  .use(mount("/", home))
-  .use(mount("/auth", auth))
-  .use(mount("/login", login))
-  .use(mount("/user", user));
+  .use(mount('/', home))
+  .use(mount('/auth', auth))
+  .use(mount('/login', login))
+  .use(mount('/user', user));
 
 export default app;
