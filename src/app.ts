bf7417ca6b8083e1e5215d531441bcc557a1405c
jannick1423 <jannick.schwartz@gmail.com>
2019-05-22 16:33:44 +0200

    feat(JS-5796): add formatError to GraphQL-response

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ const graphqlHTTP = require('koa-graphql');
 // import routes from './routes';
 // import { GraphQLSchema, GraphQLObjectType, GraphQLList } from 'graphql';
 import { RootSchema } from './graphql';
+import { GraphQLError } from 'graphql';
 // import { User } from './graphql/entity/User';
 
 export async function app() {
@@ -14,7 +15,10 @@ export async function app() {
       '/graphql',
       graphqlHTTP({
         schema: await RootSchema(),
-        graphiql: process.env.NODE_ENV !== 'production'
+        graphiql: process.env.NODE_ENV !== 'production',
+        formatError: (error: GraphQLError) => {
+          return { ...error, validationError: error.originalError };
+        }
       })
     )
   );
