30aff8997d4c2c6fe5d128ee86159b33f336423e
LenaMund <lmund@gastro-instruments.de>
2019-05-28 09:51:50 +0200

    chore(LE-1108): git merge

--- src/routes.ts
+++ src/routes.ts
@@ -4,16 +4,16 @@ import mount from 'koa-mount';
 
 import auth from './auth';
 import home from './home';
-import login from './users/login';
-import user from './users/user';
+// import login from './users/login';
+// import user from './users/user';
 
 const app = new koa();
 
 app
   .use(bodyparser())
   .use(mount('/', home))
-  .use(mount('/auth', auth))
-  .use(mount('/login', login))
-  .use(mount('/user', user));
+  .use(mount('/auth', auth));
+// .use(mount('/login', login))
+// .use(mount('/user', user));
 
 export default app;
