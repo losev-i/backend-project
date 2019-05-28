7866e53d7b55fec3f414a25f47a9b76a2bb17ae1
LenaMund <lmund@gastro-instruments.de>
2019-05-28 10:56:22 +0200

    chore(LE-1108): delete unnecessary files

--- src/home/home.ts
+++
@@ -1,6 +0,0 @@
-import { ParameterizedContext } from "koa";
-
-export async function get(ctx: ParameterizedContext, next: Function) {
-  ctx.body = "Welcome Home";
-  await next();
-}
--- src/home/routes.ts
+++
@@ -1,9 +0,0 @@
-import Router from "koa-router";
-
-import * as home from "./home";
-
-export const router = new Router();
-
-router.get("/", home.get);
-
-export default router;
