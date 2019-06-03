f07cca72ed22f65d648915d7b3e281cae198907b
LenaMund <lmund@gastro-instruments.de>
2019-06-03 10:54:03 +0200

    chore(Le-1108): Store

---
+++ src/store/configs/actions.ts
@@ -0,0 +1,13 @@
+interface ActionObject {
+  type: string;
+  payload: Object;
+}
+
+const actions: ActionObject[] = [
+  {
+    type: 'ADD_USER',
+    payload: {
+      // TODO
+    }
+  }
+];
---
+++ src/store/configs/store.ts
@@ -0,0 +1,13 @@
+import { createStore, Dispatch, Reducer } from 'redux';
+import { State } from './store.service';
+import { subscribe } from 'graphql';
+
+type Store = {
+  dispatch: Dispatch;
+  getState: () => State;
+  subscribe: (listener: () => void) => () => void;
+  replaceReducer: (reducer: Reducer) => void;
+};
+
+// TODO: Actions uebergeben
+// let store = createStore();
