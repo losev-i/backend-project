0591b72ab968e8d900c0aad4de4d28b34d85fc8b
jannick1423 <jannick.schwartz@gmail.com>
2019-06-03 11:46:44 +0200

    refactor(JS-5796): change structure add store wip

--- src/modules/shared/plain-user-object.ts
+++ src/modules/shared/plain-user-object.ts
@@ -1,23 +1,23 @@
 import { BaseEntity } from 'typeorm';
 
-import { Role } from '../users/classes/role';
+import { Role } from '../user/classes/role';
 
 export interface PlainUserInterface {
-	id: string;
-	name: string;
-	password: string;
-	email: string;
-	role: Role;
-	firstName?: string;
-	lastName?: string;
+  id: string;
+  name: string;
+  password: string;
+  email: string;
+  role: Role;
+  firstName?: string;
+  lastName?: string;
 }
 
 export class PlainUser extends BaseEntity implements PlainUserInterface {
-	id!: string;
-	name!: string;
-	email!: string;
-	role!: Role;
-	password!: string;
-	firstName?: string;
-	lastName?: string;
+  id!: string;
+  name!: string;
+  email!: string;
+  role!: Role;
+  password!: string;
+  firstName?: string;
+  lastName?: string;
 }
