636def518acaa84993899a29851b80a880f6ce6c
ReneSpringer <rene.springer@gmx.de>
2019-06-04 15:29:56 +0200

    feat(RE-1207): add logger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import koa from 'koa';
 import mount from 'koa-mount';
+import logger from 'koa-logger';
 
 import routes from './routes';
 
 export const app = new koa();
-
+app.use(logger());
 app.use(mount('/', routes));
 
 export default app;
