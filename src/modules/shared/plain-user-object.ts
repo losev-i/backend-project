cb557adca67ffe15d849f25219c7706e28703abd
jannick1423 <jannick.schwartz@gmail.com>
2019-06-11 16:29:07 +0200

    feat(JS-5796): add new EntitySchema

--- src/modules/shared/plain-user-object.ts
+++ src/modules/shared/plain-user-object.ts
@@ -1,6 +1,9 @@
 import { BaseEntity } from 'typeorm';
 
 import { Role } from '../user/classes/role';
+import { schema } from 'normalizr';
+import { Schemable } from './classes/schemable';
+import { staticImplements } from './classes/static-implements.decorator';
 
 export interface PlainUserInterface {
   id: string;
@@ -13,6 +16,19 @@ export interface PlainUserInterface {
 }
 
 export class PlainUser extends BaseEntity implements PlainUserInterface {
+  id!: string;
+  name!: string;
+  email!: string;
+  role!: Role;
+  password!: string;
+  firstName?: string;
+  lastName?: string;
+}
+
+@staticImplements<Schemable>()
+export class UserSchema {
+  static schema = new schema.Entity('user');
+
   id!: string;
   name!: string;
   email!: string;
