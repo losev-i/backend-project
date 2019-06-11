82217b7f2ad41b6f97cfc915d6007650a2cd41e2
jannick1423 <jannick.schwartz@gmail.com>
2019-06-11 16:29:43 +0200

    feat(JS-5796): add newactions and refactor some

--- src/modules/user/store/actions/user.actions.ts
+++ src/modules/user/store/actions/user.actions.ts
@@ -1,23 +1,41 @@
 import { Action } from 'redux';
 
 import { User } from '../../graphQL/user.model';
+import { createActions } from 'ngrx-normalizr-crud';
+import { UserSchema } from '../../../shared/plain-user-object';
 
-export const ADD_USER = '[User] Add User';
+export const CREATE_USER = '[User] Create User';
+
+export const READ_USER = '[User] Read User';
 
 export const UPDATE_USER = '[User] Update User';
 
 export const DELETE_USER = '[User] Delete User';
 
-export class AddUser implements Action {
+export const LOGIN_USER = '[User] Login User';
+
+export class CreateUser implements Action {
   /**
    * The action type
    */
-  public readonly type = ADD_USER;
+  public readonly type = CREATE_USER;
   /**
-   * Constructs a new ADD_USER action
+   * Constructs a new CREATE_USER action
    * @param payload The User object
    */
   constructor(public payload: User) {}
+}
+
+export class ReadUser implements Action {
+  /**
+   * The action type
+   */
+  public readonly type = READ_USER;
+  /**
+   * Constructs a new READ_USER action
+   * @param payload the string we are searching for
+   */
+  constructor(public payload: string) {}
 }
 
 export class UpdateUser implements Action {
@@ -44,18 +62,28 @@ export class DeleteUser implements Action {
   constructor(public payload: number) {}
 }
 
-/**
- * the actions about user settings
- */
-const settingsActions = {
-  ADD_USER,
-  UPDATE_USER,
-  DELETE_USER
-};
+export class LoginUser implements Action {
+  /**
+   * the action type
+   */
+  public readonly type = LOGIN_USER;
+
+  constructor(public payload: { name: string; password: string }) {}
+}
 
 /**
  * All normalizr CRUD actions and action creators
  */
 export const actions = {
-  ...settingsActions
+  ...createActions<UserSchema>(UserSchema.schema),
+  CREATE_USER,
+  CreateUser,
+  READ_USER,
+  ReadUser,
+  UPDATE_USER,
+  UpdateUser,
+  DELETE_USER,
+  DeleteUser,
+  LOGIN_USER,
+  LoginUser
 };
