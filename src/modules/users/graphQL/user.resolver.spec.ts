64c94f58965b07ed8e8d769e1dcd39c6ef50c582
ReneSpringer <rene.springer@gmx.de>
2019-06-11 08:48:19 +0200

    refactor(RE-1207): randomized user input

--- src/modules/users/graphQL/user.resolver.spec.ts
+++ src/modules/users/graphQL/user.resolver.spec.ts
@@ -3,6 +3,7 @@ import { testConnection } from '../../shared/test-utils/test-connection';
 import { Connection } from 'typeorm';
 import { call } from '../../shared/test-utils/call.graphql';
 import { ExecutionResult } from 'graphql';
+import faker from 'faker';
 import { ExecutionResultDataDefault } from 'graphql/execution/execute';
 
 let conn: Connection;
@@ -14,42 +15,52 @@ test.after(async () => {
   await conn.close();
 });
 
-const registerMutation = `mutation {
-  register(
-    firstName: "Inna",
-    lastName: "Losev",
-    email: "i515@ev.de",
-    name: "il",
-    password: "12345",
-    role: "ADMIN"
-    )  {
-    firstName
-    lastName
-    email
-    name
-    role
-  }
-}
-`;
+let registerMutation: string;
+let actual: ExecutionResult<ExecutionResultDataDefault>;
 
-// TODO: insert variables for user data, may have to change resolver
+test.beforeEach(() => {
+  const name = faker.internet.userName();
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  const email = faker.internet.email();
+  const password = faker.internet.password();
+  const role = 'USER';
+
+  registerMutation = `mutation {
+    register(
+      name: ${JSON.stringify(name)},
+      firstName: ${JSON.stringify(firstName)},
+      lastName: ${JSON.stringify(lastName)},
+      email: ${JSON.stringify(email)},
+      password: ${JSON.stringify(password)},
+      role: ${JSON.stringify(role)} 
+      )  {
+      name
+      firstName
+      lastName
+      email
+      role
+    }
+  }
+  `;
+
+  actual = {
+    data: {
+      register: {
+        name: name,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        role: role
+      }
+    }
+  };
+});
 
 test('user: register', async t => {
   const expected = await call({
     source: registerMutation
   });
-
-  let actual: ExecutionResult<ExecutionResultDataDefault> = {
-    data: {
-      register: {
-        firstName: 'Inna',
-        lastName: 'Losev',
-        email: 'i515@ev.de',
-        name: 'il',
-        role: 'ADMIN'
-      }
-    }
-  };
 
   t.deepEqual(actual, expected);
 });
