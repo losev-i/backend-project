c4849ea6b84396fd36ccf72a80922f43e254d4be
jannick1423 <jannick.schwartz@gmail.com>
2019-06-11 16:27:26 +0200

    feat(JS-5796): wip add all reducer

--- src/modules/user/store/reducers/user.reducers.ts
+++ src/modules/user/store/reducers/user.reducers.ts
@@ -1,42 +1,78 @@
 import { Action } from '@ngrx/store';
-import { initialEntityState, NormalizedEntityState } from 'ngrx-normalizr-crud';
-
-import { User } from '../../graphQL/user.model';
 import {
-  ADD_USER,
-  AddUser,
+  createReducer,
+  initialEntityState,
+  NormalizedEntityState
+} from 'ngrx-normalizr-crud';
+import { Like } from 'typeorm';
+
+import { UserSchema } from '../../../shared/plain-user-object';
+import { User } from '../../graphQL/user.model';
+import { from, Observable } from 'rxjs';
+import {
+  CREATE_USER,
+  CreateUser,
   DELETE_USER,
   DeleteUser,
+  LOGIN_USER,
+  LoginUser,
+  READ_USER,
+  ReadUser,
   UPDATE_USER,
   UpdateUser
 } from '../actions/user.actions';
 
+const entityReducer = createReducer(UserSchema.schema, initialEntityState);
+
 export type UserState = NormalizedEntityState & {
-  add: any;
+  create: any;
   delete: any;
+  read: any;
   update: any;
+  login: any;
 };
 
 /**
- * reducer function for User model
- *
+ * Reducer function for User model
+ * @param state actual state
+ * @param action action has been dispatched
  */
-export async function reducer(
+export function reducer(
   state: UserState = {
     ...initialEntityState,
-    add: null,
+    create: null,
+    read: null,
     delete: null,
-    update: null
+    update: null,
+    login: null
   },
   action: Action
 ) {
+  state = entityReducer(state, action) as UserState;
+
   switch (action.type) {
-    case ADD_USER: {
+    case CREATE_USER: {
       // Bin mir hier sehr unsicher, ob das mit async await funktioniert bei reducer
       // Hier müsste man so langsam Observables einbauen.... glaube ich
-      await User.save(action.payload);
-      return { ...state, add: (<AddUser>action).payload };
+      from(User.save((<CreateUser>action).payload));
+      return { ...state, create: (<CreateUser>action).payload };
     }
+
+    case READ_USER: {
+      const user = from(
+        User.find({
+          where: [
+            { email: Like((<ReadUser>action).payload) },
+            { name: Like((<ReadUser>action).payload) }
+          ]
+        })
+      );
+      return {
+        ...state,
+        read: user
+      };
+    }
+
     case UPDATE_USER: {
       // await User.update(action.payload);
       return {
@@ -44,13 +80,29 @@ export async function reducer(
         update: (<UpdateUser>action).payload
       };
     }
+
     case DELETE_USER: {
-      await User.delete(User, action.payload);
+      from(User.delete((<DeleteUser>action).payload));
       return {
         ...state,
         delete: (<DeleteUser>action).payload
       };
     }
+
+    case LOGIN_USER: {
+      const user = from(
+        User.find({
+          name: (<LoginUser>action).payload.name
+        })
+      );
+      if (user) {
+        return {
+          ...state,
+          login: (<LoginUser>action).payload.name
+        };
+      }
+    }
   }
+
   return state;
 }
