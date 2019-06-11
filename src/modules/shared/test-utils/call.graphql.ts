81eaf16320aed91eef4fa56db8f13399a2d883ed
ReneSpringer <rene.springer@gmx.de>
2019-06-11 13:38:19 +0200

    chore(RE-1207): remove unused code

--- src/modules/shared/test-utils/call.graphql.ts
+++ src/modules/shared/test-utils/call.graphql.ts
@@ -1,23 +1,18 @@
 import { graphql, GraphQLSchema } from 'graphql';
 import { RootSchema } from '../../users/graphQL/rootSchema.graphql';
-import Maybe from 'graphql/tsutils/Maybe';
 
 interface Options {
   source: string;
-  variableValues?: Maybe<{
-    [key: string]: any;
-  }>;
 }
 
 let schema: GraphQLSchema;
 
-export const call = async ({ source, variableValues }: Options) => {
+export const call = async ({ source }: Options) => {
   if (!schema) {
     schema = await RootSchema();
   }
   return graphql({
     schema,
-    source,
-    variableValues
+    source
   });
 };
